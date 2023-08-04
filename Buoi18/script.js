97abecb362fbdf5990be2b24e303e588dd47ac41
phamtuan162 <tnpham352@gmail.com>
2023-08-04 21:30:10 +0700

    Bài tập buổi 18

---
+++ Buoi18/script.js
@@ -0,0 +1,18 @@
+var text = document.getElementById("desc");
+var content = text.textContent;
+var arr = content.replace(/\s+/g, " ").trim().split(" ");
+console.log(arr);
+var currentIndex = 0;
+var previousIndex = 0;
+var textColor = "red";
+setInterval(function () {
+  arr[previousIndex] = arr[previousIndex].replace(textColor, "");
+
+  arr[currentIndex] = `<span class="${textColor}">${arr[currentIndex]}</span> `;
+
+  text.innerHTML = arr.join(" ");
+
+  previousIndex = currentIndex;
+
+  currentIndex = (currentIndex + 1) % arr.length;
+}, 1000);
