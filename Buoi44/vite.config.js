66d33ae221012f304bdf827c92cc347fcd0deea6
phamtuan162 <tnpham352@gmail.com>
2023-11-07 22:43:29 +0700

    update-code

--- Buoi44/vite.config.js
+++ Buoi44/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.VITE_REACT_APP_AUTH0_DOMAIN": JSON.stringify(
+      process.env.VITE_REACT_APP_AUTH0_DOMAIN
+    ),
+    "process.env.VITE_REACT_APP_AUTH0_CLIENT_ID": JSON.stringify(
+      process.env.VITE_REACT_APP_AUTH0_CLIENT_ID
+    ),
+  },
+});
