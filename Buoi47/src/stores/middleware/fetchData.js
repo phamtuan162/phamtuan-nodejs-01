c2d041274f22eab8aac3005de231e0f2575abfe1
phamtuan162 <tnpham352@gmail.com>
2023-11-19 03:23:35 +0700

    update-code

--- Buoi47/src/stores/middleware/fetchData.js
+++ Buoi47/src/stores/middleware/fetchData.js
@@ -2,7 +2,6 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getData } from "../../services/getData";
 export const fetchData = createAsyncThunk("fetchData", async () => {
   const data = await getData();
-  if (data) {
-    return data;
-  }
+
+  return data;
 });
